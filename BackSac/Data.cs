44bbf85f4987583a58d8e43460abc52b406dda9c
Imad <imad.radhi.w@gmail.com>
2023-08-27 01:39:17 +0200

    Add and modify Data file

---
+++ BackSac/Data.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackSac
+{
+    internal static class Data
+    {
+        internal static string Source = "";
+        internal static string User = "";
+        internal static bool IsAdmin = false;
+    }
+}
